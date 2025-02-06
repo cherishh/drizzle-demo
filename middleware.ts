b7eecf644f247ce194f129876ff5857b38558311
cherishh <wzx1991@gmail.com>
2025-02-06 17:21:06 +0800

    move cors settigns to nextconfig

--- middleware.ts
+++ middleware.ts
@@ -5,49 +5,16 @@ import { routing } from './i18n/routing';
 
 const isProtectedRoute = createRouteMatcher(['/dashboard(.*)', '/todo(.*)']);
 const intlMiddleware = createIntlMiddleware(routing);
-const allowedOrigins = ['https://tuxihub.com', 'https://drizzle-demo.fly.dev'];
-const corsOptions = {
-  'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
-  'Access-Control-Allow-Headers': 'Content-Type, Authorization',
-};
 
 export default clerkMiddleware(async (auth, request: NextRequest) => {
-  // Check if it's an API route
   const isApiRoute =
     request.nextUrl.pathname.startsWith('/api/') ||
     request.nextUrl.pathname.startsWith('/trpc/');
 
-  // Check the origin from the request
-  const origin = request.headers.get('origin') ?? '';
-  const isAllowedOrigin = allowedOrigins.includes(origin);
-
-  // Handle preflighted requests
-  const isPreflight = request.method === 'OPTIONS';
-
-  if (isPreflight) {
-    const preflightHeaders = {
-      ...(isAllowedOrigin && { 'Access-Control-Allow-Origin': origin }),
-      ...corsOptions,
-    };
-    return NextResponse.json({}, { headers: preflightHeaders });
-  }
-
-  // Handle simple requests
-  const response = NextResponse.next();
-
-  // Apply CORS headers
-  if (isAllowedOrigin) {
-    response.headers.set('Access-Control-Allow-Origin', origin);
-  }
-
-  Object.entries(corsOptions).forEach(([key, value]) => {
-    response.headers.set(key, value);
-  });
-
   // For API routes, apply protection and return
   if (isApiRoute) {
     if (isProtectedRoute(request)) await auth.protect();
-    return response;
+    return NextResponse.next();
   }
 
   // For non-API routes, continue with protection and internationalization
